958c17e7220254c6caf9b9ff4a96497f45a222ab
pseudotsuga-fir <hepworta@oregonstate.edu>
2022-02-06 13:07:54 -0800

    made llist_print()

--- llist.c
+++ llist.c
@@ -17,8 +17,23 @@ void node_free(struct node *n) {
 	free(n);
 }
 
-void llist_print(struct node *head){
-	printf("%d\n", head->value);
+void llist_print(struct node *head) {
+	if(!head) {
+		printf("[empty]\n");
+	}
+	while(head) {
+		printf("%d", head->value);
+		head = head->next;
+		if(head) {
+			printf(" -> ");
+		}
+	}
+	printf("\n");
+	
+}
+
+void llist_insert_tail(struct node **head, struct node *n) {
+	
 }
 
 int main(int argc, char** argv) {
@@ -29,5 +44,6 @@ int main(int argc, char** argv) {
 //	}
 	struct node *head = node_alloc(56);
 	llist_print(head);
+	node_free(head);
 
 }
