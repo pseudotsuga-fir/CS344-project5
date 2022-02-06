259b825160acae385841f0f86e3f1cf30add3af3
pseudotsuga-fir <hepworta@oregonstate.edu>
2022-02-06 12:33:46 -0800

    made node_alloc()

--- llist.c
+++ llist.c
@@ -1,5 +1,23 @@
+// Andrew Hepworth
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "llist.h"
 
-int main(int argc, char** argv) {
+struct node *node_alloc(int value) {
+	struct node *node;
+	node->value = value;
+	node->next = NULL;
+	return malloc(sizeof *node);
+}
 
+
+
+int main(int argc, char** argv) {
+	for(int i=1; i < argc; i++) {
+		if(strncmp(argv[i], "i", 1) == 0) {
+
+		}
+	}
 }
