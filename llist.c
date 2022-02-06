c62e5ec926d1cc75a14913b4a81b3d755264e509
pseudotsuga-fir <hepworta@oregonstate.edu>
2022-02-06 14:15:51 -0800

    implemented insert trail and insert head

--- llist.c
+++ llist.c
@@ -21,29 +21,52 @@ void llist_print(struct node *head) {
 	if(!head) {
 		printf("[empty]\n");
 	}
-	while(head) {
+	else {
 		printf("%d", head->value);
 		head = head->next;
-		if(head) {
-			printf(" -> ");
+		while(head) {
+			printf("-> %d", head->value);
+			head = head->next;
 		}
+		printf("\n");
 	}
-	printf("\n");
 	
+}
+
+void llist_insert_head(struct node **head, struct node *n) {
+	n->next = *head;
+	*head = n;
 }
 
 void llist_insert_tail(struct node **head, struct node *n) {
-	
+	if(!*head) {
+		*head = n;
+	}
+	else {
+		if((*head)->next) {
+			llist_insert_tail(&(*head)->next, n);
+		}
+		else {
+			(*head)->next = n;
+		}
+	}
 }
 
 int main(int argc, char** argv) {
+	struct node *head = NULL;
 //	for(int i=1; i < argc; i++) {
 //		if(strncmp(argv[i], "i", 1) == 0) {
 //
 //		}
 //	}
-	struct node *head = node_alloc(56);
+	struct node *n = node_alloc(56);
+	struct node *n2 = node_alloc(2);
+	struct node *n3 = node_alloc(68);
+	struct node *n4 = node_alloc(18);
+	llist_insert_tail(&head, n);
+	llist_insert_tail(&head, n2);
+	llist_insert_tail(&head, n3);
+	llist_insert_head(&head, n4);
 	llist_print(head);
-	node_free(head);
 
 }
